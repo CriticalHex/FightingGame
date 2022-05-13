e8994596d01f9f6cae8359ac72f9eecc34181e17
CriticalHex <kubaska05@gmail.com>
2022-05-13 13:51:13 -0600

    Everything is a function

--- FightingGame.cpp
+++ FightingGame.cpp
@@ -9,45 +9,25 @@
 
 using namespace std;
 
+void menuLoop(sf::RenderWindow& window, int winX, int winY);
+
+void gameLoop(sf::RenderWindow& window, int winX, int winY);
+
+void gameOverLoop(sf::RenderWindow& window, int winX, int winY);
+
+void eventLoop(sf::RenderWindow& window, vector<Player*>& players);
+
 int main()
 {
 	cout << "we ball.\n";
 
-	//variables
-	//window variables
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "FIGHT!", sf::Style::Fullscreen);
 	window.setFramerateLimit(60);
 	int winX = window.getSize().x;
 	int winY = window.getSize().y;
-	sf::Event event;
 
-	//timer
-	Timer timer(winX);
+	gameLoop(ref(window), winX, winY);
 
-	//text setup
-	string elapsed_time = "00:00";
-	sf::Font font;
-	font.loadFromFile("Assests/Font/arial.ttf");
-	sf::Text text(elapsed_time, font);
-	text.setCharacterSize(60);
-	text.setOrigin(text.getCharacterSize(), 0);
-	text.setPosition(winX / 2, 0);
-	text.setFillColor(sf::Color::Blue);
-
-	//bg
-	sf::Texture bg0Tex;
-	bg0Tex.loadFromFile("Assests/Background/bg0.png");
-	sf::Sprite bg0(bg0Tex);
-
-	//player initialization
-	vector<Player*> players;
-	players.push_back(new Player(sf::Vector2f(150, 600), true));
-	players.push_back(new Player(sf::Vector2f(winX - 300, 600), false));
-
-	//floor variables
-	int floorY = 900;
-	sf::Vertex floor[] = { sf::Vertex(sf::Vector2f(0, floorY), sf::Color::Red), sf::Vertex(sf::Vector2f(winX, floorY), sf::Color::Red) };
-	
 	/*bool opening = true;
 
 	while (opening == true) {
@@ -85,92 +65,38 @@ int main()
 
 
 	}*/
+	
+}
+
+void menuLoop(sf::RenderWindow& window, int winX, int winY) {
+
+}
+
+void gameLoop(sf::RenderWindow& window, int winX, int winY) {
+
+	//timer
+	Timer timer(winX);
+
+	//bg
+	sf::Texture backgroundTexture;
+	backgroundTexture.loadFromFile("Assests/Background/background.png");
+	sf::Sprite backgroundSprite(backgroundTexture);
+
+	//player initialization
+	vector<Player*> players;
+	players.push_back(new Player(sf::Vector2f(150, 600), true));
+	players.push_back(new Player(sf::Vector2f(winX - 300, 600), false));
+
+	//floor variables
+	int floorY = 900;
+	sf::Texture floorTexture;
+	floorTexture.loadFromFile("Assests/Background/floor.png");
+	sf::Sprite floorSprite(floorTexture);
+	floorSprite.setPosition(0, floorY - 100);
+
 	while (window.isOpen()) {
-		while (window.pollEvent(event))
-		{
-			// Close window: exit--------------------------------------------------------------------------------------------------------------------------
-			if (event.type == sf::Event::Closed)
-				window.close();
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) { //press lcontrol: exit
-				window.close();
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-				players[0]->keys[LEFT] = true;
-			}
-			else players[0]->keys[LEFT] = false;
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-				players[0]->keys[RIGHT] = true;
-			}
-			else players[0]->keys[RIGHT] = false;
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-				players[0]->keys[UP] = true;
-			}
-			else players[0]->keys[UP] = false;
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-				players[0]->keys[DOWN] = true;
-			}
-			else players[0]->keys[DOWN] = false;
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-				players[1]->keys[LEFT] = true;
-			}
-			else players[1]->keys[LEFT] = false;
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-				players[1]->keys[RIGHT] = true;
-			}
-			else players[1]->keys[RIGHT] = false;
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-				players[1]->keys[UP] = true;
-			}
-			else players[1]->keys[UP] = false;
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-				players[1]->keys[DOWN] = true;
-			}
-			else players[1]->keys[DOWN] = false;
-
-
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::G)) {
-				players[0]->quickAttack(ref(window), players[1]);
-			}
-
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) {
-				players[0]->heavyAttack();
-			}
-
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y)) {
-				players[0]->block();
-			}
-
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::H)) {
-				players[0]->specialAttack();
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad1)) {
-				players[1]->quickAttack(ref(window), players[0]);
-			}
-
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad2)) {
-				players[1]->specialAttack();
-			}
-
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad3)) {
-				players[1]->heavyAttack();
-			}
-
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad5)) {
-				players[1]->block();
-			}
-
-		}
+		eventLoop(ref(window), ref(players));
 
 		for (auto& it : players) {
 			if (it->getHealth() <= 0) {
@@ -186,12 +112,105 @@ int main()
 
 		//render
 		window.clear();
-		window.draw(bg0);
+		window.draw(backgroundSprite);
 		timer.count(ref(window));
-		window.draw(floor, 2, sf::Lines);
+		window.draw(floorSprite);
 		for (auto& it : players) {
 			it->draw(ref(window));
 		}
 		window.display();
-	};
+	}
+}
+
+void gameOverLoop(sf::RenderWindow& window, int winX, int winY) {
+
+}
+
+void eventLoop(sf::RenderWindow& window, vector<Player*>& players) {
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		// Close window: exit--------------------------------------------------------------------------------------------------------------------------
+		if (event.type == sf::Event::Closed)
+			window.close();
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) { //press lcontrol: exit
+			window.close();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+			players[0]->keys[LEFT] = true;
+		}
+		else players[0]->keys[LEFT] = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+			players[0]->keys[RIGHT] = true;
+		}
+		else players[0]->keys[RIGHT] = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+			players[0]->keys[UP] = true;
+		}
+		else players[0]->keys[UP] = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+			players[0]->keys[DOWN] = true;
+		}
+		else players[0]->keys[DOWN] = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+			players[1]->keys[LEFT] = true;
+		}
+		else players[1]->keys[LEFT] = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+			players[1]->keys[RIGHT] = true;
+		}
+		else players[1]->keys[RIGHT] = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+			players[1]->keys[UP] = true;
+		}
+		else players[1]->keys[UP] = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			players[1]->keys[DOWN] = true;
+		}
+		else players[1]->keys[DOWN] = false;
+
+
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::G)) {
+			players[0]->quickAttack(ref(window), players[1]);
+		}
+
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) {
+			players[0]->heavyAttack();
+		}
+
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y)) {
+			players[0]->block();
+		}
+
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::H)) {
+			players[0]->specialAttack();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad1)) {
+			players[1]->quickAttack(ref(window), players[0]);
+		}
+
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad2)) {
+			players[1]->specialAttack();
+		}
+
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad3)) {
+			players[1]->heavyAttack();
+		}
+
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad5)) {
+			players[1]->block();
+		}
+
+	}
 }
